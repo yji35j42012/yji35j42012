7b3400c6fd3f29bf4cab40aab9699b361945c5af
yji35j4212 <yji35j4212@gmail.com>
2021-12-17 17:39:33 +0800

    檔案同步

--- template/Day6/api/setData.php
+++ template/Day6/api/setData.php
@@ -6,21 +6,29 @@
 	$data = json_decode($content , true);// 把JSON字串轉成PHP陣列
 	$done ="false";
 
-	$sql = "INSERT INTO `stock_list`(  `stock_name`, `date`, `merchandise`, `stock_price`, `stock_count`,`stock_handling`) 
-			VALUES ('". $data["stockName"] ."','" . $data["addTime"] . "'," . $data["merchandise"] . ",". $data["price"] . "," . $data["count"] . ",". $data["handling"] .")";
-
-
-
+	$sql = "INSERT INTO `evaluate`(  `evaluate_store`, `evaluate_menu`, `evaluate_eat`, `evaluate_action`, `evaluate_amount`, `evaluate_experience`, `evaluate_user`, `evaluate_date`) 
+			VALUES ('". 
+			$data["evaluate_store"] ."','" . 
+			$data["evaluate_menu"] . "'," . 
+			$data["evaluate_eat"] . ",". 
+			$data["evaluate_action"] . "," . 
+			$data["evaluate_amount"] . ",'". 
+			$data["evaluate_experience"] . "','". 
+			$data["evaluate_user"] . "','". 
+			$data["evaluate_date"] ."')";
+			
+echo($sql);
 	mysqli_query($link,$sql);
 
-	$getSql = "SELECT * FROM `stock_list`";
-	$result = mysqli_query($link,$getSql );
-	$arr=array();
-	while($row=mysqli_fetch_row($result)){
-		$arr[]= $row;
-	}
-	mysqli_free_result($result);
-	echo (json_encode($arr));
+	// $getSql = "SELECT * FROM `evaluate`";
+	// $result = mysqli_query($link,$getSql );
+	// $arr=array();
+	// while($row=mysqli_fetch_row($result)){
+	// 	$arr[]= $row;
+	// }
+	// mysqli_free_result($result);
+	// echo (json_encode($arr));
+
 	// echo($sql);
     // mysqli_free_result($result);
 
--- template/Day6/api/updateData.php
+++ template/Day6/api/updateData.php
@@ -6,22 +6,29 @@
 	$data = json_decode($content , true);// 把JSON字串轉成PHP陣列
 	$done ="false";
 
-	$sql = "INSERT INTO `stock_list`(  `stock_name`, `date`, `merchandise`, `stock_price`, `stock_count`,`stock_handling`) 
-			VALUES ('". $data["stockName"] ."','" . $data["addTime"] . "'," . $data["merchandise"] . ",". $data["price"] . "," . $data["count"] . ",". $data["handling"] .")";
+	// $sql = "INSERT INTO `evaluate`(  `evaluate_store`, `evaluate_menu`, `evaluate_eat`, `evaluate_action`, `evaluate_amount`, `evaluate_experience`, `evaluate_user`, `evaluate_date`) 
+	// 		VALUES ('". 
+	// 		$data["evaluate_store"] ."','" . 
+	// 		$data["evaluate_menu"] . "'," . 
+	// 		$data["evaluate_eat"] . ",". 
+	// 		$data["evaluate_action"] . "," . 
+	// 		$data["evaluate_amount"] . ",'". 
+	// 		$data["evaluate_experience"] . "','". 
+	// 		$data["evaluate_user"] . "','". 
+	// 		$data["evaluate_date"] ."')";
+	$sql =" UPDATE `evaluate` SET 
+			`evaluate_store`='".$data["evaluate_store"]."' , 
+			`evaluate_menu`='".$data["evaluate_menu"]."' , 
+			`evaluate_eat`='".$data["evaluate_eat"]."', 
+			`evaluate_action`='".$data["evaluate_action"]."',
+			`evaluate_amount`='".$data["evaluate_amount"]."',
+			`evaluate_experience`='".$data["evaluate_experience"]."',
+			`evaluate_user`='".$data["evaluate_user"]."',
+			`evaluate_date`='".$data["evaluate_date"]."' WHERE `evaluate_id` = ".$data["evaluate_id"];
 
-
-
+			// UPDATE `evaluate` SET `evaluate_store`=[value-2],`evaluate_menu`=[value-3],`evaluate_eat`=[value-4],`evaluate_action`=[value-5],`evaluate_amount`=[value-6],`evaluate_experience`=[value-7],`evaluate_user`=[value-8],`evaluate_date`=[value-9] WHERE 1
+			
+	echo($sql);
 	mysqli_query($link,$sql);
-
-	$getSql = "SELECT * FROM `stock_list`";
-	$result = mysqli_query($link,$getSql );
-	$arr=array();
-	while($row=mysqli_fetch_row($result)){
-		$arr[]= $row;
-	}
-	mysqli_free_result($result);
-	echo (json_encode($arr));
-	// echo($sql);
-    // mysqli_free_result($result);
 
 ?>
