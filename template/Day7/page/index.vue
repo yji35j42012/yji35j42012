7278f952df0cfc44feeae06f1370c137eedc621a
yji35j4212 <yji35j4212@gmail.com>
2021-12-29 14:52:30 +0800

    day7 70分鐘

---
+++ template/Day7/page/index.vue
@@ -0,0 +1,37 @@
+<style scoped></style>
+
+<template>
+    <div class="container">
+        <div class="banner">
+            <div
+                class="banner_item"
+                style="background-image:url('./images/banner.jpg')"
+            >
+                <div class="banner_txt">
+                    <p class="banner_txt_num">05</p>
+                    <p class="banner_txt_title">Twin Room</p>
+                    <p class="banner_txt_info">
+                        潔淨白色極簡主義,姿意享受舒適與自在,身心靈極致美好的享受
+                    </p>
+                    <p class="banner_txt_price">
+                        <span class="normal">$3,350</span>
+                        <span class="holiday">假日($3,500)</span>
+                    </p>
+                    <button class="normal_btn">線上訂房</button>
+                </div>
+            </div>
+        </div>
+        <footer>Copyright ©2021 FunRoom. All rights reserved.</footer>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {}
+    },
+    mounted() {},
+    computed: {},
+    methods: {},
+}
+</script>
