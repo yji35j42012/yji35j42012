20d2c4c3e78bf28be1f5224ccd65a322e1f6eadb
yji35j4212 <yji35j4212@gmail.com>
2022-08-24 17:03:31 +0800

    檔案同步

---
+++ todolist/js/icon_all.js
@@ -0,0 +1,5 @@
+var icon_all = {
+    list: "M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z",
+    equalizer: "M10 20h4V4h-4v16zm-6 0h4v-8H4v8zM16 9v11h4V9h-4z",
+    nightlife:"M1,5h14l-6,9v4h2v2H5v-2h2v-4L1,5z M10.1,9l1.4-2H4.49l1.4,2H10.1z M17,5h5v3h-3v9h0c0,1.66-1.34,3-3,3s-3-1.34-3-3 s1.34-3,3-3c0.35,0,0.69,0.06,1,0.17L17,5z"
+};
