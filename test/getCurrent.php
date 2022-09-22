cafb2e0325dffec270e0f325a060e5b552db027a
yji35j4212 <yji35j4212@gmail.com>
2022-09-22 10:45:19 +0800

    檔案同步

--- test/getCurrent.php
+++ test/getCurrent.php
@@ -1,10 +1,27 @@
 <?php 
+    $db_host = 'localhost';
+    $db_name = 'id18087221_experience';
+    $db_user = 'id18087221_experience_data';
+    $db_pass = 'J?b*i5)PQ\ru{\|t';
 
-    
-    $rData =  readfile("https://www.taifex.com.tw/cht/3/dailyFXRateDown/?queryStartDate=".$_GET['queryStartDate']."&queryEndDate=".$_GET['queryEndDate']);
+    $db_charset = 'utf8';
+    $db_collate = 'utf8_unicode_ci';
+    $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
+    $pdo_options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_PERSISTENT => false,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$db_charset} COLLATE
+        {$db_collate}"
+    ];
+    try {
+        $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+        echo 'suss';
+    } catch(PDOException $ex) {
+        echo 'Connection failed:'. $ex->getMessage();
+    }
 
-    echo $rData;
 
-    echo(json_encode($rData));//這裡用echo而不是return
 
 ?>
--- test/index1.php
+++ test/index1.php
@@ -1,6 +1,39 @@
 <?php 
 
-  $_GET['']
-  $rData = readfile("https://www.taifex.com.tw/cht/3/dailyFXRateDown/?queryStartDate=2022/07/01&queryEndDate=2022/08/01");
-  echo(json_encode($rData));//這裡用echo而不是return
+	require __DIR__. '/getCurrent.php';
+	echo '<br>';
+	echo 'index1 start';
+	$t_sql ="SELECT COUNT(1)FROM `evaluate` WHERE 1";
+	echo '<br>';
+	echo 'p_sql'.$p_sql;
+	$per_page=4;
+	$total_rows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+	echo '<br>';
+	echo "total_rows:".$total_rows;
+	$total_pages = ceil($total_rows/$per_page);
+	// $sql = "SELECT * FROM `evaluate` WHERE 1";
+	// $stmt = $pdo->prepare($sql);
+	// echo '$stmt'.$stmt;
+	// $stmt->execute([
+	// 	$_POST['evaluate_store'],
+	// ]);
+	echo '<br>';
+	echo "total_pages:".$total_pages;
+	echo '<br>';
+	// $count = $stmt->rowCount();
+	echo "count".$count;
+
+
+	$sql = "SELECT * FROM `evaluate` WHERE 1"; 
+	$stmt = $pdo->query($sql);
+	// $row = $stmt->fetch(PDO::FETCH_ASSOC);
+	// $row = $stmt->fetch(PDO::FETCH_ASSOC);
+	while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+		echo '<br>';
+		echo "row:".$row['evaluate_store'];
+	}
+
+	// $p_sql = sprintf("SELECT * FROM products $where LIMIT %s, %s ",
+	// ($page-1)*$per_page, $per_page);
+	// $p_stmt = $pdo->query($p_sql);
 ?>
