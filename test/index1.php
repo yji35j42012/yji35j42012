01b85759719593b4d74a2266b335c3056662da3f
yji35j4212 <yji35j4212@gmail.com>
2022-09-23 10:37:35 +0800

    檔案同步

--- test/index1.php
+++ test/index1.php
@@ -36,4 +36,82 @@
 	// $p_sql = sprintf("SELECT * FROM products $where LIMIT %s, %s ",
 	// ($page-1)*$per_page, $per_page);
 	// $p_stmt = $pdo->query($p_sql);
+	$count = [
+		'{
+			"districts": [
+				{
+					"zip": "100",
+					"name": "中正區"
+				},
+				{
+					"zip": "103",
+					"name": "大同區"
+				},
+				{
+					"zip": "104",
+					"name": "中山區"
+				},
+				{
+					"zip": "105",
+					"name": "松山區"
+				},
+				{
+					"zip": "106",
+					"name": "大安區"
+				},
+				{
+					"zip": "108",
+					"name": "萬華區"
+				},
+				{
+					"zip": "110",
+					"name": "信義區"
+				},
+				{
+					"zip": "111",
+					"name": "士林區"
+				},
+				{
+					"zip": "112",
+					"name": "北投區"
+				},
+				{
+					"zip": "114",
+					"name": "內湖區"
+				},
+				{
+					"zip": "115",
+					"name": "南港區"
+				},
+				{
+					"zip": "116",
+					"name": "文山區"
+				}
+			],
+			"name": "臺北市"
+		}',
+	];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+<?php
+      $name = [
+          "臺北市" => ["中正區","大同區","中山區","松山區","大安區","萬華區","信義區","士林區","北投區","內湖區","南港區","文山區"],
+          "新北市" => [],
+      ];
+  ?>
+ 	<select name="" id="">
+    	<?php foreach ( $name["臺北市"] as $key => $value ) { ?>
+		<option value=""><?php echo $value ?></option>
+        <?php } ?>
+	</select>
+</body>
+</html>
