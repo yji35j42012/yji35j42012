3f2b5036a783d37364859863078916c12a42eb12
yji35j4212 <yji35j4212@gmail.com>
2023-02-15 18:05:16 +0800

    檔案同步

---
+++ template/Day14/page/home.vue
@@ -0,0 +1,28 @@
+<style scoped></style>
+
+<template>
+	<div class="container">
+		<div class="menu">
+			<div class="dayinfo">
+				<div class="menu_time">1:30</div>
+				<div class="menu_temperature msg_right" data-msgr="°F">72</div>
+			</div>
+			<button class="close">關閉</button>
+			<div class="menu_note">
+				Extra cool people meeting
+				<span>10AM</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+module.exports = {
+	data() {
+		return {};
+	},
+	mounted() {},
+	computed: {},
+	methods: {}
+};
+</script>
