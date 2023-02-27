9c4a423d3bca7b66202d63c1a3a82ac2cae91d9a
yji35j4212 <yji35j4212@gmail.com>
2023-02-27 10:17:31 +0800

    檔案同步

---
+++ template/Day14/page/index.vue
@@ -0,0 +1,23 @@
+<style scoped></style>
+
+<template>
+    <div class="container">
+        <div class="login_info">
+            <span class="login_info_time">9:59</span>
+            <span class="login_info_temperature msg_right" data-msgr="°F">82</span>
+        </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {
+            icon_all: icon_all
+        };
+    },
+    mounted() {},
+    computed: {},
+    methods: {}
+};
+</script>
---
+++ template/Day14/page/login.vue
@@ -0,0 +1,20 @@
+<style scoped></style>
+
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {
+            icon_all: icon_all
+        };
+    },
+    mounted() {},
+    computed: {},
+    methods: {}
+};
+</script>
