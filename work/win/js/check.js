eb8e910106973c4a37609e1570bee62019db9145
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2024-02-26 08:54:39 +0800

    檔案同步

---
+++ work/win/js/check.js
@@ -0,0 +1,10 @@
+alert('browserW:' + document.documentElement.scrollWidth
++ '\nbrowserH:' + document.documentElement.scrollHeight
++ '\nreport_list1W:' + document.querySelectorAll('.report_list')[0].offsetWidth
++ '\nreport_list1H:' + document.querySelectorAll('.report_list')[0].offsetHeight
++ '\nreport_list2W:' + document.querySelectorAll('.report_list')[1].offsetWidth
++ '\nreport_list2H:' + document.querySelectorAll('.report_list')[1].offsetHeight
++ '\nreport_list3W:' + document.querySelectorAll('.report_list')[2].offsetWidth
++ '\nreport_list4H:' + document.querySelectorAll('.report_list')[2].offsetHeight
++ '\nscreenW:' + screen.width
++ '\nscreenH:' + screen.height);
