9abd084687ebdc92fa6618ba3a21beb76f045b60
yji35j42012 <yji35j42012@gmail.com>
2024-09-27 11:08:52 +0800

    檔案同步

---
+++ components/RightPanel.vue
@@ -0,0 +1,37 @@
+<style scoped></style>
+
+<template>
+	<div class="content_r">
+		<div class="marquee">
+			<i class="icon_msg" v-html="icon_all.icon_msg"></i>
+			<marquee scrollamount="5" direction="left" class="marquee_box">
+				<span>hahaha</span>
+			</marquee>
+		</div>
+
+		<div class="ad">
+			<div class="ad_box"></div>
+		</div>
+		<div class="ad">
+			<div class="ad_box"></div>
+		</div>
+		<div class="ad">
+			<div class="ad_box"></div>
+		</div>
+		<div class="ad">
+			<div class="ad_box"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+module.exports={
+	data() {
+		return {
+			icon_all: icon_all,
+		}
+	},
+	computed: {},
+	methods: {},
+}
+</script>
