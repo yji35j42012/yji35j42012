b624d9f8fec472912bfd11e597a8165b44f9b920
yji35j42012 <yji35j42012@gmail.com>
2025-04-09 14:05:48 +0800

    檔案同步

--- page/live.vue
+++ page/live.vue
@@ -1,6 +1,21 @@
 <style scoped></style>
 
-<template>live</template>
+<template>
+	<div class="sportHead">
+		<div class="sportHead_league">
+			<div class="sportHead_menu"></div>
+			<div class="sportHead_title">
+				<div class="sportHead_title_txt">
+					<div class="league">
+						<i class="icon_arr"></i>
+						<span data-showleague="/聯盟">電子競技</span>
+					</div>
+					<div class="name">滾球賽事</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
 
 <script>
 module.exports = {
