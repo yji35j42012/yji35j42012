983b28198e1c93d4315defbe7aeb0c7c09f2f628
yji35j42012 <yji35j42012@gmail.com>
2025-05-12 18:12:05 +0800

    檔案同步

--- page/home.vue
+++ page/home.vue
@@ -4,11 +4,25 @@
 	<div class="home_title"><span>滾球中賽事</span></div>
 	<sport-btn></sport-btn>
 	<div class="home_title"><span>體育</span></div>
-	<!-- <div class="home_sport">
-		<div class="home_sport_item">
-
-		</div>
-	</div> -->
+	<div class="home_sport">
+		<template v-for="index in 20" :key="index">
+			<div class="home_sport_item">
+				<div class="home_sport_title">足球</div>
+				<div class="home_sport_pic">
+					<img src="/images/home/img_home_ft.jpg" alt="">
+				</div>
+				<div class="home_sport_btn">
+					<div class="home_sport_new">
+						<span>滾球</span>
+						<div class="home_sport_num">
+							1
+							<i class="icon_homearr" v-html="icon_all.icon_homearr"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</template>
+	</div>
 </template>
 
 <script>
